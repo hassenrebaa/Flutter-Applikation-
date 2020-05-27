bf3c74b4a38830032222453b80ba6ab337b801f2
Med Yassin Azlouk <azlouknour@gmail.com>
2020-05-27 23:46:05 +0200

    Test

--- lib/Chatview.dart
+++ lib/Chatview.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-
-
-
 class ChatDetails extends StatefulWidget {
   @override
   _ChatDetailsState createState() => _ChatDetailsState();
 }
-
 class _ChatDetailsState extends State<ChatDetails> {
   @override
   Widget build(BuildContext context) {
