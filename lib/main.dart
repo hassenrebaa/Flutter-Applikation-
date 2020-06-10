40ffaf65ad5787292c598a73309e26fa0df1498a
Morchid, Abdelhamid <abdelhamid.morchid001@stud.fh-dortmund.de>
2020-06-10 23:30:14 +0200

    Merge remote-tracking branch 'origin/master'
                    # Conflicts:
    
                    #	lib/main.dart
                    #	lib/test-save.dart
                    #	pubspec.lock
                    #	pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/test-save.dart';
 import 'Einstellung.dart';
-import 'Layout.dart';
 import 'Model-json.dart';
 import 'chat.dart';
 
